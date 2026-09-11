31a202eb8f7a3a1dec1091622b29783dc128e043
MaroneFed <martin.federer08@gmail.com>
2026-09-11 19:16:21 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE="marone-scout-v2";
+var CACHE="marone-scout-v3";
 var FILES=["./","./index.html","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",function(e){self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(FILES);}));});
@@ -7,7 +7,14 @@ self.addEventListener("activate",function(e){
     return n===CACHE?null:caches.delete(n);}));}).then(function(){return self.clients.claim();}));});
 self.addEventListener("fetch",function(e){
   if(e.request.method!=="GET") return;
-  e.respondWith(caches.match(e.request).then(function(h){
-    return h||fetch(e.request).then(function(r){var c=r.clone();
-      caches.open(CACHE).then(function(x){x.put(e.request,c);});return r;
-    }).catch(function(){return caches.match("./index.html");});}));});
+  var u;
+  try{ u=new URL(e.request.url); }catch(err){ return; }
+  if(u.origin!==self.location.origin) return;
+  e.respondWith(caches.match(e.request).then(function(hit){
+    return hit||fetch(e.request).then(function(r){
+      var c=r.clone();
+      caches.open(CACHE).then(function(x){x.put(e.request,c);});
+      return r;
+    }).catch(function(){ return caches.match("./index.html"); });
+  }));
+});
